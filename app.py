d6e317556a359ee5d469a31b5f2d26ad4c344973
Anise D'Orange Ferreira <aniseferreira@users.noreply.github.com>
2026-05-11 21:27:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ email = st.text_input("Email")
 
 st.markdown(
     """
-#### Associe cada oração em grego antigo à sua tradução em português.
+#### Selecione do menu uma tradução em português para cada oração em grego antigo.
 """
 )
 
